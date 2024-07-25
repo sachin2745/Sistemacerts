3efbe958f81b1926aa4c59cf2e747f771ebd101c
sachin2745 <sachin.official648@gmail.com>
2024-07-25 11:22:40 +0530

    why choose us is designed

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,11 +3,13 @@ import Header from './(main)/Header/page'
 import Footer from './(main)/Footer/page'
 import CallToAction from './(main)/Calltoaction/page'
 import Stats from './(main)/Stats/page'
+import WhyChooseUs from './(main)/Whychooseus/page'
 
 const page = () => {
   return (
     <>
       <Header />
+      <WhyChooseUs />
       <Stats />
       <CallToAction />
       <Footer />
