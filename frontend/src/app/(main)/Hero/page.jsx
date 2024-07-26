f41ac61c1d6fb27310c88d59cb5f391dc56942ed
sachin2745 <sachin.official648@gmail.com>
2024-07-26 12:21:36 +0530

    error solve

--- frontend/src/app/(main)/Hero/page.jsx
+++ frontend/src/app/(main)/Hero/page.jsx
@@ -36,27 +36,27 @@ const Hero = () => {
                                 Learn More<MdOutlineKeyboardDoubleArrowRight className='mt-1 ' />
                             </button> */}
                             <button
-                                class="relative flex items-center px-6 py-3 overflow-hidden font-medium transition-all bg-tri rounded-md group"
+                                className="relative flex items-center px-6 py-3 overflow-hidden font-medium transition-all bg-tri rounded-md group"
                             >
                                 <span
-                                    class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-tri rounded group-hover:-mr-4 group-hover:-mt-4"
+                                    className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-tri rounded group-hover:-mr-4 group-hover:-mt-4"
                                 >
                                     <span
-                                        class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"
+                                        className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"
                                     ></span>
                                 </span>
                                 <span
-                                    class="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-tri rounded group-hover:-ml-4 group-hover:-mb-4"
+                                    className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-tri rounded group-hover:-ml-4 group-hover:-mb-4"
                                 >
                                     <span
-                                        class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"
+                                        className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"
                                     ></span>
                                 </span>
                                 <span
-                                    class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-tri rounded-md group-hover:translate-x-0"
+                                    className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-tri rounded-md group-hover:translate-x-0"
                                 ></span>
                                 <span
-                                    class="relative flex w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white"
+                                    className="relative flex w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white"
                                 > Learn More<MdOutlineKeyboardDoubleArrowRight className='mt-1 ' /></span
                                 >
                             </button>
