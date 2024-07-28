c10171ebb34c3556012584b62aad99db6ba0995b
sachin2745 <sachin.official648@gmail.com>
2024-07-28 17:03:30 +0530

    preloader is setup

---
+++ frontend/src/app/(main)/Preloader/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./preloader.css"
+
+const Preloader = () => {
+    return (
+        <>
+            <div className="banter-loader">
+                <div className="banter-loader__box" />
+                <div className="banter-loader__box" />
+                <div className="banter-loader__box" />
+                <div className="banter-loader__box" />
+                <div className="banter-loader__box" />
+                <div className="banter-loader__box" />
+                <div className="banter-loader__box" />
+                <div className="banter-loader__box" />
+                <div className="banter-loader__box" />
+            </div>
+        </>
+    )
+}
+
+export default Preloader
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './(main)/Header/page'
 import Footer from './(main)/Footer/page'
 import CallToAction from './(main)/Calltoaction/page'
@@ -10,8 +10,23 @@ import About from './(main)/About/page'
 import Services from './(main)/Services/page'
 import Hero from './(main)/Hero/page'
 import Certification from './(main)/Certification/page'
+import Preloader from './(main)/Preloader/page';
 
 const page = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust the timeout duration as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Preloader />;
+  }
+
   return (
     <>
       <Header />
